032a93013f1af1d998784a616bd5a379c408dfaf
Theo Hervouet <thervoue@z2r3p5.le-101.fr>
2019-07-16 16:35:01 +0200

    lol

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: thervoue <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/07/16 13:23:59 by thervoue     #+#   ##    ##    #+#       */
-/*   Updated: 2019/07/16 13:24:12 by thervoue    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/07/16 16:33:11 by thervoue    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,12 +16,14 @@ char		*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
 	while (i < n)
 	{
-		if (src[i] != '\0')
-			dest[i] = src[i];
-		else
-			dest[i] = '\0';
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
