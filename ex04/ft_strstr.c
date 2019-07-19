e915e935e45934d3e3c83bb264ecd5e7b2655868
Theo Hervouet <thervoue@z2r3p3.le-101.fr>
2019-07-19 12:14:24 +0200

    dans ta mere

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: thervoue <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/07/18 13:09:51 by thervoue     #+#   ##    ##    #+#       */
-/*   Updated: 2019/07/18 19:24:04 by thervoue    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/07/19 12:13:36 by thervoue    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,7 +14,7 @@
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
-	intj;
+	int j;
 
 	i = 0;
 	if (to_find[0] == '\0')
